0c142fd38974901c4b6506af6918d671e9bbfcb3
Bhavesh <143629789+bhavesh2327@users.noreply.github.com>
2026-01-20 12:23:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (25.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,36 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s),end(s));
-        sort(begin(t),end(t));
+        // approach 1 
+        // sort(begin(s),end(s));
+        // sort(begin(t),end(t));
 
-        return s == t ;
+        // return s == t ;
+
+        // approach 2 
+        // Step 1: Create an array to store count of each letter (a–z)
+vector<int> count(26, 0);
+
+
+for (char ch : s) {
+    count[ch - 'a']++;
+}
+
+
+for (char ch : t) {
+    count[ch - 'a']--;
+}
+
+
+for (int i = 0; i < 26; i++) {
+    if (count[i] != 0) {
+        return false;  
+    }
+}
+
+return true;  
+
+
 
     }
 };
